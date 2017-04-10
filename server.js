9723fdd0bbcfc33aa51d579341747f8244bd4502
Nuno Tavares <Nuno@Nunos-MacBook-Pro.local>
2017-04-10 18:13:04 -0400

    Added port for heroku

--- server.js
+++ server.js
@@ -9,8 +9,12 @@ var Article = require("./models/Article.js");
 // Our scraping tools
 var request = require("request");
 var cheerio = require("cheerio");
+
+var PORT = process.env.PORT || 3000;
+
 // Set mongoose to leverage built in JavaScript ES6 Promises
 mongoose.Promise = Promise;
+
 
 
 // Initialize Express
@@ -166,7 +170,7 @@ app.post("/articles/:id", function(req, res) {
 
 
 // Listen on port 3000
-app.listen(3000, function() {
+app.listen(PORT, function() {
   console.log("App running on port 3000!");
 });
 
